49ba40b1181a812929ad8812502277cb8fc03658
Victor Giovanni Beltrán Rodríguez <vgbr.dev@gmail.com>
2023-04-18 11:56:18 -0600

    build: add commitlint config file

---
+++ commitlint.config.js
@@ -0,0 +1,8 @@
+/**
+ * @file Manages the commitlint module configuration.
+ */
+
+// ━━	EXPORT MODULE	━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+};
